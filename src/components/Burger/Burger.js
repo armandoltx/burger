a80a28e3d771cef8a4e81991501f9cf01d332824
Armando Ruiz <armando.ruiz@industrie.co>
2018-08-29 09:42:35 +1000

    Making BurgerIngredient dynamic

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,11 +4,19 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
+    // To convert the state created in BurgerBuilder to an array to use it in here
+  const transformedIngredients = Object.keys(props.ingredients)
+      .map(igKey => {
+          // to convert the string value into an array that contains as many elements as ingredients we have
+          return [...Array(props.ingredients[igKey])].map((_), i) => {
+            return <BurgerIngredient key={igKey + 1} type={igKey};
+          };
+          // use (_) because we want to get the index, we do not care about the element itself
+      });
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
-      <BurgerIngredient type="cheese" />
-      <BurgerIngredient type="meat" />
+      {transformedIngredients}
       <BurgerIngredient type="bread-bottom" />
 
     </div>
